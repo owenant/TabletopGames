1a0a5fb4b3a50a00ce9a08f6411762d56ceaa014
owenant <anthonyowen1@gmail.com>
2023-08-13 22:38:13 +0100

    refactored GA code

---
+++ src/main/java/games/dominion/metrics/DominionDeckGenome.java
@@ -0,0 +1,227 @@
+package games.dominion.metrics;
+
+import core.AbstractGameState;
+import core.AbstractPlayer;
+import core.Game;
+import core.actions.AbstractAction;
+import core.components.PartialObservableDeck;
+import games.GameType;
+import games.dominion.DominionConstants.DeckType;
+import games.dominion.DominionFGParameters;
+import games.dominion.DominionForwardModel;
+import games.dominion.DominionGameState;
+import games.dominion.cards.CardType;
+import games.dominion.cards.DominionCard;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import players.mcts.MCTSPlayer;
+
+public class DominionDeckGenome implements Comparable<DominionDeckGenome>{
+  private String genotype;
+  private int[] phenotype;
+  private double fitness;
+  private boolean fitnessCalculated;
+  public static int MAX_NO_OF_CARDS_OF_ANY_TYPE = 10;
+  public static int NO_SIMULATIONS_EXPPAYOFF = 10;
+  public static int COST_CONSTRAINT = 30;
+  public static CardType[] CARD_TYPES = {CardType.CELLAR, CardType.MARKET, CardType.MERCHANT,CardType.MILITIA,
+      CardType.MINE,CardType.MOAT,CardType.SMITHY,CardType.VILLAGE,CardType.GOLD,
+      CardType.SILVER, CardType.COPPER};
+
+  public DominionDeckGenome(String genome){
+    genotype = new String(genome);
+
+    //convert genotype of ones and zeros to phenotype, i.e vector of integers
+    //representing number of cards of each type
+
+    //number of bits per integer is dependent on integer upper bound
+    int noBitsPerInt = (int)Math.floor(Math.log(MAX_NO_OF_CARDS_OF_ANY_TYPE)/Math.log(2)+1);
+
+    //check genotype is compatible with maxInt constraint
+    if (genotype.length() != CARD_TYPES.length * noBitsPerInt){
+      System.out.println("Error: incompatible genotype, cant convert to phenotype");
+    }
+
+    //parse genotype in groups of noBitsPerInt bits
+    phenotype = new int[CARD_TYPES.length];
+    for (int i = 0; i < CARD_TYPES.length; i++){
+      String singleIntSubString = genotype.substring(i*noBitsPerInt, (i+1)*noBitsPerInt);
+      //convert bits back to integer
+      phenotype[i] = Integer.parseInt(singleIntSubString, 2);
+    }
+
+    fitnessCalculated = false;
+  }
+
+  public DominionDeckGenome(int[] phenome){
+    phenotype = phenome;
+
+    //convert phenotype - i.e. vector of integers representing number of each type of
+    // card in deck to genotype which is a vector of ones and zeros
+
+    //check phenotype is compatible with maxInt constraint
+    for(int i = 0; i < phenotype.length; i++){
+      if (phenotype[i] > MAX_NO_OF_CARDS_OF_ANY_TYPE){
+        System.out.println("Error - maxInt constraint violated");
+      }
+    }
+
+    //number of bits per integer is dependent on integer upper bound
+    int noBitsPerInt = (int)Math.floor(Math.log(MAX_NO_OF_CARDS_OF_ANY_TYPE)/Math.log(2)+1);
+
+    //convert each integer to a sequence of four bits and add to genotype
+    String genotype = new String();
+    String allele = new String();
+    for(int i = 0; i < phenotype.length; i++){
+      allele = Integer.toBinaryString(phenotype[i]);
+      while(allele.length() < noBitsPerInt){
+        //pad with zeros so we have noBitsPerInt bits for ever integer
+        allele = "0" + allele;
+      }
+      genotype += allele;
+    }
+
+    fitnessCalculated = false;
+  }
+  public double getFitness(){
+    if (fitnessCalculated = false){
+      fitness = calcFitness();
+      fitnessCalculated = true;
+    }
+    return fitness;
+  }
+
+  public String getGenotype(){
+    return genotype;
+  }
+
+  public int compareTo(DominionDeckGenome otherGenome){
+    if (fitness > otherGenome.fitness){
+      return 1;
+    }else if (fitness < otherGenome.fitness){
+      return -1;
+    }else{
+      return 0;
+    }
+  }
+  private double calcFitness(){
+    //provides fitness function for deck
+    //for now driven by treasures, but could be update to give credit to things like
+    //card trashing or automatically gaining a new card etc. They could be translated into
+    //an approximate treasure worth
+
+    //check to see if genotype is compatible with cost constraint
+    if (getCost() != COST_CONSTRAINT){
+      return 0;
+    }else {
+      //set-up Dominion game and state
+      List<AbstractPlayer> players = Arrays.asList(new MCTSPlayer(), new MCTSPlayer());
+      DominionFGParameters params = new DominionFGParameters();
+      DominionGameState state = new DominionGameState(params, players.size());
+      DominionForwardModel fm = new DominionForwardModel();
+      //note creating a game initialises the state
+      Game game = new Game(GameType.Dominion, players, fm, state);
+
+      //create player draw deck
+      boolean[] notVisibleToEitherPlayer = {false, false};
+      //focus player is the player whose deck we will be evolving
+      int focusPlayer = state.getCurrentPlayer();
+      PartialObservableDeck<DominionCard> draw = new PartialObservableDeck<DominionCard>("player_draw", focusPlayer, notVisibleToEitherPlayer);
+      for(int i =0; i < phenotype.length; i++) {
+        for(int j = 0; j < phenotype[i]; j++){
+          draw.add(DominionCard.create(CARD_TYPES[i]),notVisibleToEitherPlayer);
+        }
+      }
+      state.setDeck(DeckType.DRAW,focusPlayer,draw);
+
+      //make sure there are no cards in hand
+      boolean[] visibleToFocusPlayer = {true, false};
+      PartialObservableDeck<DominionCard> hand = new PartialObservableDeck<DominionCard>("player_hand", focusPlayer, visibleToFocusPlayer);
+      state.setDeck(DeckType.HAND,focusPlayer,hand);
+
+      //store a copy of this starting state for this deck
+      DominionGameState startState = (DominionGameState) state.copy();
+
+      int noObsOfPayOffs = 0;
+      double expectedPayout = 0;
+      while(noObsOfPayOffs <= NO_SIMULATIONS_EXPPAYOFF){
+        //if start of round shuffle draw deck and draw cards
+        if(state.getGamePhase() == DominionGameState.DominionGamePhase.Play && state.getCurrentPlayer() == focusPlayer &&
+            state.getDeck(DeckType.HAND, focusPlayer).getSize() == 0) {
+          //shuffle draw deck for focus player
+          state.getDeck(DeckType.DRAW, focusPlayer).shuffle(new Random(System.currentTimeMillis()));
+
+          //start by drawing cards into hand
+          for (int i = 0; i < params.HAND_SIZE; i++) {
+            state.drawCard(focusPlayer);
+          }
+        }
+
+        //observe current state
+        AbstractGameState observation = state.copy(focusPlayer);
+        List<AbstractAction> possibleActions = fm.computeAvailableActions(observation);
+        //TODO: could probably speed this up alot by using a random AI or a small rollout number for MCTS
+        //as the AI just needs to play the existing cards, and not look ahead
+        AbstractAction aiAction = players.get(focusPlayer)
+            .getAction(observation, possibleActions);
+
+        //if in buy phase for focus player figure out what the payoff was
+        if (observation.getGamePhase() == DominionGameState.DominionGamePhase.Buy && state.getCurrentPlayer() == focusPlayer){
+          //grab total treasure value
+          int payout = state.availableSpend(focusPlayer);
+          expectedPayout += payout;
+
+          //reset state, ready to repeat
+          state = (DominionGameState) startState.copy();
+
+          //keep a counter for a sense check
+          noObsOfPayOffs += 1;
+        }else{
+          //otherwise perform the AI action
+          fm.next(state, aiAction);
+        }
+      }
+      //compute final expected payoff
+      expectedPayout = expectedPayout/(noObsOfPayOffs*1.0);
+
+      return expectedPayout;
+    }
+  }
+  public int getCost() {
+    int cost = 0;
+    for (int i = 0; i < phenotype.length; i++){
+      cost += phenotype[i] * CARD_TYPES[i].cost;
+    }
+    return cost;
+  }
+  public static ArrayList<DominionDeckGenome> crossOver(DominionDeckGenome parent1, DominionDeckGenome parent2){
+    //cross-over genes of two parents to make two new off spring
+
+    //we split both genotypes down the middle and create new off spring
+    int midPoint = (int)Math.floor(parent1.genotype.length()/2);
+    String child1 = parent1.genotype.substring(0,midPoint) + parent2.genotype.substring(midPoint, parent2.genotype.length());
+    String child2 = parent2.genotype.substring(0,midPoint) + parent1.genotype.substring(midPoint, parent1.genotype.length());
+
+    ArrayList<DominionDeckGenome> children = new ArrayList<DominionDeckGenome>();
+    children.add(DominionDeckGenome(child1));
+    children.add(DominionDeckGenome(child2));
+
+    return children;
+  }
+  public static DominionDeckGenome mutate(DominionDeckGenome genome){
+    //randomly flip a bit in genotype and return a new genome
+    Random rnd = new Random(System.currentTimeMillis());
+    int rndIndex = rnd.nextInt(genome.genotype.length());
+
+    String newGenotype = new String();
+    if(genome.genotype.charAt(rndIndex) == '0'){
+      newGenotype = genome.genotype.substring(0,rndIndex) + '1' + genome.genotype.substring(rndIndex+1, genotype.length());
+    }else{
+      newGenotype = genome.genotype.substring(0,rndIndex) + '0' + genome.genotype.substring(rndIndex+1, genotype.length());
+    }
+
+    return new DominionDeckGenome(newGenotype);
+  }
+}
--- src/main/java/games/dominion/metrics/MetricsForDBCGs.java
+++ src/main/java/games/dominion/metrics/MetricsForDBCGs.java
@@ -60,200 +60,102 @@ public class MetricsForDBCGs {
       // of 17 genes which are integers representing the number of cards of a given
       //card type in the deck. Here we are assuming we are using the base collection of cards
 
-      //no of simulations for computing expected deck payoff
-      int noSims = 20;
-
-      //set-up vector to  convert chromosome index to card type
-      CardType[] indexToType = new CardType[11];
-      indexToType[0] = CardType.CELLAR;
-      indexToType[1] = CardType.MARKET;
-      indexToType[2] = CardType.MERCHANT;
-      indexToType[3] = CardType.MILITIA;//will impact, but full benefit wont be recognised
-      indexToType[4] = CardType.MINE;
-      indexToType[5] = CardType.MOAT; //will impact, but full benefit wont be recognised
-      //indexToType[6] = CardType.REMODEL; //wont impact treasure
-      indexToType[6] = CardType.SMITHY;
-      indexToType[7] = CardType.VILLAGE;
-      //indexToType[9] = CardType.WORKSHOP;//wont impact treasure
-      indexToType[8] = CardType.GOLD;
-      indexToType[9] = CardType.SILVER;
-      indexToType[10] = CardType.COPPER;
-
-      //convert to string of ones and zeros
-      int maxNoCardsOfGivenType = 10;
-      int[] test1 = {1,3,5,0,0,1,6,8,3,0,1};
-      int[] test2 = {0,6,3,7,5,2,4,8,0,3,1};
-      String genotype1 = convertIntVecToBitGenes(test1, maxNoCardsOfGivenType);
-      String genotype2 = convertIntVecToBitGenes(test2, maxNoCardsOfGivenType);
-      int[] phenotype = convertBitGenesToIntVec(genotype1, maxNoCardsOfGivenType, indexToType.length);
-      //create crossover offspring that still obeys cost constraint
-      ArrayList<String> children = crossOver(genotype1, genotype2);
-      int len1 = genotype1.length();
-      int len2 = genotype2.length();
-      int len3 = children.get(0).length();
-      int len4 = children.get(1).length();
-      //create mutated child
-      int seedmut = 32;
-      String mutatedChild = mutation(genotype1, seedmut);
-      int[] mutatedChildPheno = convertBitGenesToIntVec(mutatedChild, maxNoCardsOfGivenType, indexToType.length);
-
       //create initial population at random filtering out those that dont satisfy
       //the cost constraints
       int noIndividuals = 50;
-      long seed = 100;
       int maxIterations = 100000;
-      Set<int[]> initialPop = genInitialPopulation(totalCost, noIndividuals, indexToType,
-          seed, maxIterations);
+      long seed = 100;
+      ArrayList<DominionDeckGenome> parents = genInitialPopulation(noIndividuals, maxIterations, seed);
 
-      //create a map that contains the fitness of each member of the population
-      Map<int[], Double> popFitness = new HashMap<int[], Double>();
-      for ( int[] sample : initialPop){
-          popFitness.put(sample, fitness(sample, indexToType, noSims, seed));
-      }
+      //start loop to evolve population, finish when termination condition is achieved
+      Random rnd = new Random(System.currentTimeMillis());
+      double probCrossOver = 0.8;
+      double probMutation = 0.05;
+      while (){
+          ArrayList<DominionDeckGenome> children = new ArrayList<DominionDeckGenome>();
+          for( int pair = 0; pair < (int)Math.floor(parents.size()/2); pair++ ) {
+              //draw pairs of individuals randomly from the population
+              DominionDeckGenome parent1 = drawFromPopulation(parents);
+              DominionDeckGenome parent2 = drawFromPopulation(parents);
 
-      //select parents - probability based on fitness function
-      int[] parent1 = drawFromPopulation(popFitness, seed*2);
-      int[] parent2 = drawFromPopulation(popFitness, seed*3);
-
-      //create cross-over
-
-      //apply max entropy mutation
-
-      //note: use max entropy for mutations and penalty function for cost constraint.
-      //fitness function is given by expected deck pay off
-      //can I use JGAP package? Not sure I need to....
-  }
-
-  public static String mutation(String parent, long seed){
-      //randomly flip a bit in parent to produce a child
-      Random rnd = new Random(seed);
-      int rndIndex = rnd.nextInt(parent.length());
-
-      String child = new String();
-      if(parent.charAt(rndIndex) == '0'){
-          child = parent.substring(0,rndIndex) + '1' + parent.substring(rndIndex+1, parent.length());
-      }else{
-          child = parent.substring(0,rndIndex) + '0' + parent.substring(rndIndex+1, parent.length());
-      }
-
-      return child;
-  }
-
-  public static ArrayList<String> crossOver(String parent1, String parent2){
-      //cross-over genes of two parents to make two new off spring
-
-      //we split both genotypes down the middle and create new off spring
-      int midPoint = (int)Math.floor(parent1.length()/2);
-      String child1 = parent1.substring(0,midPoint) + parent2.substring(midPoint, parent2.length());
-      String child2 = parent2.substring(0,midPoint) + parent1.substring(midPoint, parent1.length());
-
-      ArrayList<String> children = new ArrayList<String>();
-      children.add(child1);
-      children.add(child2);
-
-      return children;
-
-  }
-  public static int[] convertBitGenesToIntVec(String genotype, int maxInt, int noOfCardTypes){
-      //convert genotype of ones and zeros back to phenotype, i.e vector of integers
-      //representing number of cards of each type
-
-      //number of bits per integer is dependent on integer upper bound
-      int noBitsPerInt = (int)Math.floor(Math.log(maxInt)/Math.log(2)+1);
-
-      //check genotype is compatible with maxInt constraint
-      if (genotype.length() != noOfCardTypes * noBitsPerInt){
-          System.out.println("Error: incompatible genotype, cant convert to phenotype");
-          return null;
-      }
-
-      //parse genotype in groups of noBitsPerInt bits
-      int[] phenotype = new int[noOfCardTypes];
-      for (int i = 0; i < noOfCardTypes; i++){
-          String singleIntSubString = genotype.substring(i*noBitsPerInt, (i+1)*noBitsPerInt);
-          //convert bits back to integer
-          phenotype[i] = Integer.parseInt(singleIntSubString, 2);
-      }
-
-      return phenotype;
-  }
-  public static String convertIntVecToBitGenes(int[] phenotype, int maxInt){
-      //convert phenotype - i.e. vector of integers representing number of each type of
-      // card in deck to genotype which is a vector of ones and zeros
-
-      //check phenotype is compatible with maxInt constraint
-      for(int i = 0; i < phenotype.length; i++){
-          if (phenotype[i] > maxInt){
-              return "Error - maxInt constraint violated";
+              //cross-over pairs with a probability probCrossOver
+              DominionDeckGenome child1 = new DominionDeckGenome(parent1.getGenotype());
+              DominionDeckGenome child2 = new DominionDeckGenome(parent2.getGenotype());
+              if (rnd.nextFloat() < probCrossOver){
+                  ArrayList<DominionDeckGenome> childList = DominionDeckGenome.crossOver(parent1, parent2);
+                  child1 = childList.get(0);
+                  child2 = childList.get(1);
+              }
+              //mutate each new child with probability probMutation
+              if(rnd.nextFloat() < probMutation){
+                  child1 = DominionDeckGenome.mutate(child1);
+              }
+              if(rnd.nextFloat() < probMutation){
+                  child2 = DominionDeckGenome.mutate(child2);
+              }
+              //add to child population
+              children.add(child1);
+              children.add(child2);
           }
-      }
 
-      //number of bits per integer is dependent on integer upper bound
-      int noBitsPerInt = (int)Math.floor(Math.log(maxInt)/Math.log(2)+1);
-
-      //convert each integer to a sequence of four bits and add to genotype
-      String genotype = "";
-      String allele = "";
-      for(int i = 0; i < phenotype.length; i++){
-          allele = Integer.toBinaryString(phenotype[i]);
-          while(allele.length() < noBitsPerInt){
-              //pad with zeros so we have noBitsPerInt bits for ever integer
-              allele = "0" + allele;
+          //once child population is complete combine with previous population to generate next generation
+          for (DominionDeckGenome child : children){
+              parents.add(child);
           }
-          genotype += allele;
-      }
 
-      return genotype;
+          //sort population by fitness
+
+
+          //and take the top noIndividuals in terms of fitness as the new population
+
+
+          //examine fitessed individual out of new population and compare to termination
+          //condition
+
+          //check size of next generation population
+
+      }
   }
-  public static int[] drawFromPopulation(Map<int[], Double> populationFitness, long seed){
+
+  public static DominionDeckGenome drawFromPopulation(ArrayList<DominionDeckGenome> population){
       //draw a sample from a population using the relative fitness of each sample
       //compared ot the whole population
-      Random rnd = new Random(seed);
+      Random rnd = new Random(System.currentTimeMillis());
 
       //total fitness values across population
       double totalfitness = 0;
-      for(Map.Entry<int[], Double> entry : populationFitness.entrySet()){
-          totalfitness += entry.getValue();
+      for(DominionDeckGenome genome : population){
+          totalfitness += genome.getFitness();
       }
 
       //randomly select a sample from the population
-      for(Map.Entry<int[], Double> entry : populationFitness.entrySet()){
-          double probOfSelection = entry.getValue()/totalfitness;
+      for(DominionDeckGenome genome : population){
+          double probOfSelection = genome.getFitness()/totalfitness;
           if (rnd.nextFloat() < probOfSelection){
-              return entry.getKey();
+              return genome;
           }
       }
 
       //if nothing chosen, just choose a random entry in the map
-      //get list of keys
-      List<int[]> poplist = new ArrayList<int[]>(populationFitness.keySet());
-      return poplist.get(rnd.nextInt(poplist.size()));
+      return population.get(rnd.nextInt(population.size()));
   }
-  public static Set<int[]> genInitialPopulation(int costConstraint, int numberOfIndividuals,
-      CardType[] cardTypes, long seed, int maxIterations){
+  public static ArrayList<DominionDeckGenome> genInitialPopulation(int numberOfIndividuals, int maxIterations, long seed)
+  {
       //generate random samples keeping only those that satisfy the cost constraint
       //set a maximum number of copies for any type of action card
       Random rnd = new Random(seed);
-      int maxNoActionCardsOfAnyType = 5;
-      //also set maximum for any type of treasure card
-      int maxNoTreasureCardsOfAnyType = 10;
       int noOfSamplesGenerated = 0;
       //keep a track of total number of randomly generated decks
       int decksGenerated = 0;
-      Set<int[]> pop = new HashSet<int[]>();
+      ArrayList<DominionDeckGenome> pop = new ArrayList<DominionDeckGenome>();
       while(noOfSamplesGenerated < numberOfIndividuals){
-          int[] deck = new int[cardTypes.length];
-          for(int i = 0; i < cardTypes.length; i++){
-              if (cardTypes[i] == CardType.COPPER || cardTypes[i] == CardType.SILVER
-                  || cardTypes[i] == CardType.GOLD){
-                  deck[i] = rnd.nextInt(maxNoTreasureCardsOfAnyType);
-              }else{
-                  deck[i] = rnd.nextInt(maxNoActionCardsOfAnyType);
-              }
+          int[] deck = new int[DominionDeckGenome.CARD_TYPES.length];
+          for(int i = 0; i < DominionDeckGenome.CARD_TYPES.length; i++){
+              deck[i] = rnd.nextInt(DominionDeckGenome.MAX_NO_OF_CARDS_OF_ANY_TYPE);
           }
-          int cost = deckCost(deck, cardTypes);
-          if(cost == costConstraint){
-              pop.add(deck);
+          DominionDeckGenome genome = new DominionDeckGenome(deck);
+          if(genome.getCost() == DominionDeckGenome.COST_CONSTRAINT){
+              pop.add(genome);
               noOfSamplesGenerated += 1;
           }
 
@@ -266,100 +168,6 @@ public class MetricsForDBCGs {
           }
       }
       return pop;
-  }
-
-  public static int deckCost(int[] deckComposition, CardType[] cardTypes) {
-      int cost = 0;
-      for (int i = 0; i < deckComposition.length; i++){
-          cost += deckComposition[i] * cardTypes[i].cost;
-      }
-      return cost;
-  }
-
-  public static double fitness(int[] deckComposition, CardType[] cardTypes, int noSimulations, long seed){
-      //provides fitness function for deck
-      //for now driven by treasures, but could be update to give credit to things like
-      //card trashing or automatically gaining a new card etc. They could be translated into
-      //an approximate treasure worth
-
-      double result = expectedDeckPayoff(deckComposition, cardTypes, noSimulations, seed);
-
-      return result;
-  }
-
-  public static double expectedDeckPayoff(int[] deckComposition, CardType[] cardTypes, int noSimulations, long seed)
-  {
-      //set-up Dominion game and state
-      List<AbstractPlayer> players = Arrays.asList(new MCTSPlayer(), new MCTSPlayer());
-      DominionFGParameters params = new DominionFGParameters(seed);
-      DominionGameState state = new DominionGameState(params, players.size());
-      DominionForwardModel fm = new DominionForwardModel();
-      //note creating a game initialises the state
-      Game game = new Game(GameType.Dominion, players, fm, state);
-
-      //create player draw deck
-      boolean[] notVisibleToEitherPlayer = {false, false};
-      //focus player is the player whose deck we will be evolving
-      int focusPlayer = state.getCurrentPlayer();
-      PartialObservableDeck<DominionCard> draw = new PartialObservableDeck<DominionCard>("player_draw", focusPlayer, notVisibleToEitherPlayer);
-      for(int i =0; i < deckComposition.length; i++) {
-          for(int j = 0; j < deckComposition[i]; j++){
-              draw.add(DominionCard.create(cardTypes[i]),notVisibleToEitherPlayer);
-          }
-      }
-      state.setDeck(DeckType.DRAW,focusPlayer,draw);
-
-      //make sure there are no cards in hand
-      boolean[] visibleToFocusPlayer = {true, false};
-      PartialObservableDeck<DominionCard> hand = new PartialObservableDeck<DominionCard>("player_hand", focusPlayer, visibleToFocusPlayer);
-      state.setDeck(DeckType.HAND,focusPlayer,hand);
-
-      //store a copy of this starting state for this deck
-      DominionGameState startState = (DominionGameState) state.copy();
-
-      int noObsOfPayOffs = 0;
-      double expectedPayout = 0;
-      while(noObsOfPayOffs <= noSimulations){
-          //if start of round shuffle draw deck and draw cards
-          if(state.getGamePhase() == DominionGameState.DominionGamePhase.Play && state.getCurrentPlayer() == focusPlayer &&
-              state.getDeck(DeckType.HAND, focusPlayer).getSize() == 0) {
-            //shuffle draw deck for focus player
-            state.getDeck(DeckType.DRAW, focusPlayer).shuffle(new Random(System.currentTimeMillis()));
-
-            //start by drawing cards into hand
-            for (int i = 0; i < params.HAND_SIZE; i++) {
-              state.drawCard(focusPlayer);
-            }
-          }
-
-          //observe current state
-          AbstractGameState observation = state.copy(focusPlayer);
-          List<AbstractAction> possibleActions = fm.computeAvailableActions(observation);
-          //TODO: could probably speed this up alot by using a random AI or a small rollout number for MCTS
-          //as the AI just needs to play the existing cards, and not look ahead
-          AbstractAction aiAction = players.get(focusPlayer)
-              .getAction(observation, possibleActions);
-
-          //if in buy phase for focus player figure out what the payoff was
-          if (observation.getGamePhase() == DominionGameState.DominionGamePhase.Buy && state.getCurrentPlayer() == focusPlayer){
-              //grab total treasure value
-              int payout = state.availableSpend(focusPlayer);
-              expectedPayout += payout;
-
-              //reset state, ready to repeat
-              state = (DominionGameState) startState.copy();
-
-              //keep a counter for a sense check
-              noObsOfPayOffs += 1;
-          }else{
-              //otherwise perform the AI action
-              fm.next(state, aiAction);
-          }
-      }
-      //compute final expected payoff
-      expectedPayout = expectedPayout/(noObsOfPayOffs*1.0);
-
-      return expectedPayout;
   }
 
   public static void runTournament(){
